090f7568caeae84d953cc90807382b197687f692
vladimirruppel <vladimir.ruppel2004@mail.ru>
2023-10-02 19:03:22 +0700

    Добавлены команды в меню в main()

--- prog3_lab2_1-c.cpp
+++ prog3_lab2_1-c.cpp
@@ -60,6 +60,7 @@ void audioCollectionRemoveElement(AudioCollection* obj, int index);
 AudioFileArray* audioCollectionSearchByArtistName(AudioCollection* obj, char const* artistName);
 AudioFileArray* audioCollectionSearchByReleaseYear(AudioCollection* obj, int releaseYear);
 void audioCollectionPrintStats(AudioCollection* obj);
+void audioCollectionFree(AudioCollection* obj);
 
 typedef struct {
     Playlist* currPlaylist;
@@ -82,6 +83,7 @@ MusicLibrary* musicLibraryCreate();
 AudioFile* musicLibraryCreateAudioFile(MusicLibrary* lib);
 Playlist* musicLibraryCreatePlaylist(MusicLibrary* lib);
 int musicLibraryPrintAudioFileArray(MusicLibrary* lib);
+int musicLibraryPrintPlaylistArray(MusicLibrary* lib);
 void musicLibraryFree(MusicLibrary* lib);
 
 int main()
@@ -97,13 +99,18 @@ int main()
         puts("1. Создать аудиофайл");
         puts("2. Создать плейлист");
         puts("3. Вывести список созданных треков");
+        puts("4. Вывести список плейлистов");
+        puts("5. Поиск треков по имени исполнителя");
+        puts("6. Поиск треков по году выхода");
+        puts("7. Редактировать плейлист");
         puts("0. Выход");
 
         int command;
         do {
             printf("Введите команду: ");
             scanf("%d", &command);
-        } while (command < 0 || command > 3);
+            while (getchar() != '\n');
+        } while (command < 0 || command > 7);
 
         switch (command) {
         case 0:
@@ -112,15 +119,153 @@ int main()
             break;
         case 1:
             puts("Создать аудиофайл");
-            while (getchar() != '\n');
             musicLibraryCreateAudioFile(lib);
             break;
-        case 2:
+        case 2: 
             puts("Создать плейлист");
             musicLibraryCreatePlaylist(lib);
+            break;
         case 3:
             puts("Вывести список созданных треков");
             musicLibraryPrintAudioFileArray(lib);
+            break;
+        case 4:
+            puts("Вывести список плейлистов");
+            musicLibraryPrintPlaylistArray(lib);
+            break;
+        case 5: {
+            puts("Поиск треков по имени исполнителя");
+
+            if (lib->audioFileArray->listLength == 0) {
+                puts("Поиск невозможен: нет треков в библиотекe");
+                break;
+            }
+
+            char* artistName;
+            artistName = (char*)calloc(30, sizeof(char));
+            if (!artistName)
+                puts(ERROR_MESSAGE);
+
+            printf("Введите имя исполнителя: ");
+            gets_s(artistName, 30);
+
+            AudioCollection* collection = audioCollectionCreate(lib->audioFileArray);
+            AudioFileArray* arr = audioCollectionSearchByArtistName(collection, artistName);
+
+            if (arr->listLength == 0)
+                puts("Не найдено треков по введенному имени исполнителя");
+            else {
+                puts("Найденные треки: ");
+                audioFileArrayShortOutput(arr);
+            }
+
+            free(artistName);
+            audioCollectionFree(collection);
+        } break;
+        case 6: {
+            puts("Поиск треков по году выхода");
+
+            if (lib->audioFileArray->listLength == 0) {
+                puts("Поиск невозможен: нет треков в библиотекe");
+                break;
+            }
+
+            int releaseYear;
+
+            printf("Введите год выхода трека: ");
+            scanf("%d", &releaseYear);
+
+            AudioCollection* collection = audioCollectionCreate(lib->audioFileArray);
+            AudioFileArray* arr = audioCollectionSearchByReleaseYear(collection, releaseYear);
+
+            if (arr->listLength == 0)
+                puts("Не найдено треков по введенному году выхода");
+            else {
+                puts("Найденные треки: ");
+                audioFileArrayShortOutput(arr);
+            }
+
+            audioCollectionFree(collection);
+        } break;
+        case 7: {
+            puts("Редактирование плейлиста");
+
+            if (lib->playlistArray->listLength == 0) {
+                puts("Редактирование плейлиста невозможно: нет плейлистов в библиотеке");
+            }
+            else {
+                musicLibraryPrintPlaylistArray(lib);
+
+                int playlistNumber;
+                do {
+                    printf("Введите номер плейлиста для редактирования: ");
+                    scanf("%d", &playlistNumber);
+                } while (playlistNumber < 1 || playlistNumber > lib->playlistArray->listLength);
+
+                Playlist* chosenPlaylist = &lib->playlistArray->list[playlistNumber - 1];
+                printf("Выбранный плейлист: %s\n", chosenPlaylist->playlistName);
+
+                puts("Меню работы с плейлистом");
+                puts("1. Изменение названия плейлиста");
+                puts("2. Добавление трека в плейлист");
+                puts("3. Удаление трека из плейлиста");
+                puts("4. Вывод списка треков плейлиста");
+                puts("0. Завершить редактирование плейлиста");
+
+                int answer;
+                do {
+                    do {
+                        printf("Выбранный пункт: ");
+                        scanf("%d", &answer);
+                    } while (answer < 1 || answer > 4);
+
+                    switch (answer) {
+                    case 0:
+                        puts("Завершение работы по редактированию плейлиста");
+                        break;
+                    case 1: {
+                        puts("Изменение названия плейлиста");
+
+                        char* playlistName;
+                        playlistName = (char*)calloc(30, sizeof(char));
+                        if (!playlistName)
+                            puts(ERROR_MESSAGE);
+
+                        free(chosenPlaylist->playlistName);
+                        chosenPlaylist->playlistName = _strdup(playlistName);
+
+                        printf("Название плейлиста изменено на \"%s\"\n", chosenPlaylist->playlistName);
+                        free(playlistName);
+                    } break;
+                    case 2: {
+                        puts("Добавление трека в плейлист");
+                        AudioFile* audioFile = musicLibraryCreateAudioFile(lib);
+                        playlistAddElement(chosenPlaylist, audioFile);
+                        puts("Трек добавлен в плейлист");
+                    } break;
+                    case 3: {
+                        puts("Удаление трека из плейлиста");
+
+                        audioFileArrayShortOutput(chosenPlaylist->list);
+
+                        int chosenTrackNumber;
+                        do {
+                            printf("Введите номер трека, который желаете удалить: ");
+                            scanf("%d", &chosenTrackNumber);
+                        } while (chosenTrackNumber < 1 || chosenTrackNumber > chosenPlaylist->list->listLength);
+
+                        playlistRemoveElement(chosenPlaylist, chosenTrackNumber - 1);
+                        puts("Выбранный трек удален из плейлиста");
+                    } break;
+                    case 4:
+                        puts("Вывод списка треков плейлиста");
+                        audioFileArrayShortOutput(chosenPlaylist->list);
+                        break;
+                    }
+                } while (answer != 0);
+            }
+
+        } break;
         }
     } while (!quit);
 }
@@ -304,7 +449,7 @@ int playlistArrayRemoveElement(PlaylistArray* arr, int index)
 int playlistArrayOutput(PlaylistArray* arr) {
     puts("Список плейлистов:");
     for (int i = 0; i < arr->listLength; i++) {
-        printf("%d. %s", (i + 1), (&arr->list[i])->playlistName);
+        printf("%d. %s\n", (i + 1), (&arr->list[i])->playlistName);
     }
     return arr->listLength;
 }
@@ -381,6 +526,11 @@ void audioCollectionPrintStats(AudioCollection* obj)
 
     puts("Статистика музыкальной коллекции:");
     printf("Количество треков: %d\nДлительность всех треков: %d\nСредняя длительность: %lf", tracksCount, fullDuration, averageDuration);
+}
+
+void audioCollectionFree(AudioCollection* obj)
+{
+    free(obj);
 }
 
 // методы для структуры AudioPlayer
@@ -482,6 +632,9 @@ Playlist* musicLibraryCreatePlaylist(MusicLibrary* lib)
     if (!playlistName)
         puts(ERROR_MESSAGE);
 
+    printf("Введите имя плейлиста: ");
+    gets_s(playlistName, 30);
+
     AudioFileArray* arr = audioFileArrayCreate();
     int toContinueChoosing = 1;
 
@@ -496,6 +649,7 @@ Playlist* musicLibraryCreatePlaylist(MusicLibrary* lib)
         do {
             printf("Выбор (0-2): ");
             scanf("%d", &choosingAns);
+            while (getchar() != '\n');
         } while (choosingAns < 0 || choosingAns > 2);
 
         if (choosingAns == 0) { // выход из цикла
@@ -542,6 +696,15 @@ int musicLibraryPrintAudioFileArray(MusicLibrary* lib)
     else
         audioFileArrayShortOutput(lib->audioFileArray);
     return lib->audioFileArray->listLength;
+}
+
+int musicLibraryPrintPlaylistArray(MusicLibrary* lib)
+{
+    if (lib->playlistArray->listLength == 0)
+        puts("Выводить нечего: нет плейлистов в библиотеке");
+    else 
+        playlistArrayOutput(lib->playlistArray);
+    return lib->playlistArray->listLength;
 }
 
 void musicLibraryFree(MusicLibrary* lib)
